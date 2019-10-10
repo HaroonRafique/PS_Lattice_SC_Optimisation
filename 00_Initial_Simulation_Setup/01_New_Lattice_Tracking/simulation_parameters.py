fe8e68213e9941a759ae9db289ccb25dcc9dc905
Haroon Rafique <haroon.rafique@protonmail.com>
2019-10-10 15:27:59 +0200

    00: updated simulation parameters

--- 00_Initial_Simulation_Setup/01_New_Lattice_Tracking/simulation_parameters.py
+++ 00_Initial_Simulation_Setup/01_New_Lattice_Tracking/simulation_parameters.py
@@ -23,14 +23,9 @@ rf_voltage = 0.0212942055190595723
 # Simulation Parameters
 n_macroparticles = int(0.5E6)
 turns_max = int(100)
-tu1 = range(-1, turns_max, 200)
-tu2 = range(10, 100, 10) 
-tu3 = range(1, 9)
-tu = tu2 + tu1 + tu3 
-tu.append(874) # WS 172s
-tu.append(2185)# WS 175s
+tu = range(-1, turns_max, 1)
 
-turns_print = [874, 2185]
+turns_print = [0, 99, 100]
 turns_update = sorted(tu)
 macrosize = intensity/float(n_macroparticles)
 
